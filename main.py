d3ce45792f3569e1aca23b72477419432f0c1690
sagarmamodia <sagar74822@gmail.com>
2023-01-26 19:08:33 +0530

    restarted the whole project

--- main.py
+++ main.py
@@ -3,36 +3,35 @@ from sys import exit
 from pygame.math import Vector2 as vec2
 from settings import *
 from snake import *
-from block import *
 
 
 class Game:
     def __init__(self):
         pg.init()
         self.display = pg.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-        self.display.fill(WHITE)
-        self.snake = Snake(self.display) #Created a snake object
-
-    def run(self):
-        
-        while True:
-            for event in pg.event.get():
-                if event.type == pg.QUIT:
-                    exit()
-            
-            self.update()
-            pg.display.update()
-    
-    def update(self):
-        self.draw_grid()
-        self.snake.update() #updating snake at every frame
 
     def draw_grid(self):
         #vertical lines
         for i in range(1, int(SCREEN_WIDTH/BLOCK_SIZE)):
-            pg.draw.line(self.display, BLACK, (i * BLOCK_SIZE, 0), (i*BLOCK_SIZE, SCREEN_HEIGHT))
+            pg.draw.line(self.display, GREY, (i * BLOCK_SIZE, 0), (i*BLOCK_SIZE, SCREEN_HEIGHT))
+        #Horizontal lines
         for i in range(1, int(SCREEN_HEIGHT/BLOCK_SIZE)):
-            pg.draw.line(self.display, BLACK, (0, i * BLOCK_SIZE), (SCREEN_WIDTH, i * BLOCK_SIZE))
+            pg.draw.line(self.display, GREY, (0, i * BLOCK_SIZE), (SCREEN_WIDTH, i * BLOCK_SIZE))
+      
+    def run(self):  
+        while True:
+            self.display.fill(BLACK)
+            self.update()
+            pg.display.update()
+
+    def check_events(self):
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                exit()
+    
+    def update(self):
+        self.check_events()
+        self.draw_grid()
 
 
 if __name__ == '__main__':
--- snake.py
+++ snake.py
@@ -1,17 +1,11 @@
 import pygame as pg
 from pygame.math import Vector2 as vec2
 from settings import *
-from block import *
 
 class Snake():
     def __init__(self, display):
-        self.display = display
-        self.length = SNAKE_LENGTH
-        self.blocks = [Block(i) for i in range(SNAKE_LENGTH)]
-    
-    def draw(self):
-        for block in self.blocks:
-            block.draw(self.display, RED, (200, 200), BLOCK_SIZE)
-    
+       pass
+        
     def update(self):
-        self.draw()
+        pass
+
